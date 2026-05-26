bcad6d0cdf6bd54823f415f56d8b1e6794ab8d5c
mantenext1-code <mantenext1@gmail.com>
2026-05-25 21:45:40 -0300

    Ejercicios tomy y vicky

--- src/main/java/Starapi/ApiApplication.java
+++ src/main/java/Starapi/ApiApplication.java
@@ -96,6 +96,126 @@ public class ApiApplication {
 		System.out.println("El ataque más poderoso es: " + Ncontador + " con un daño de: " + contador);
 		System.out.println("------Siguiente punto------");
 
+		// =========================================================================
+        // Ejercicio 5   
+        // =========================================================================
+		int contador1=0;
+		int scont = 0;
+		int n=0;
+		for (Ataque ataque : Shelly.getAtaques() ){
+			contador1 = ataque.getCostoEnergia();
+			scont= scont + contador1;
+			n=n+1;
+		}
+		int resultado = scont/n;
+		if (resultado > 50){
+			System.out.println("el promedio de energía de los ataques de Shelly es: " + resultado + " es mayor a 50");	
+		}
+		else{
+			System.out.println("el promedio de energía de los ataques de Shelly es: " + resultado + " es menor a 50");
+		}
+		System.out.println("------Siguiente punto------");
+
+		CalcularDanio(Shelly, Colt, golpe);
+
+		System.out.println("------Siguiente punto------");
+		Descanso(Colt);
+
+		System.out.println("------Siguiente punto------");
+
+		FiltrarHabilidades(Shelly);
+
+		System.out.println("------Siguiente punto------");
+
+		// =========================================================================
+        // Ejercicio 10
+        // =========================================================================
+
+		while (Shelly.getPuntosVida() > 0 && Colt.getPuntosVida() > 0){
+			CalcularDanio(Shelly, Colt, Shelly.getAtaques().get(0));
+			if (Colt.getPuntosVida() <= 0 || Shelly.getPuntosVida() <= 0){
+				break;
+			}
+			else{
+				Descanso(Shelly);
+			}
+			CalcularDanio(Colt, Shelly, Colt.getAtaques().get(0));
+			if (Colt.getPuntosVida() <= 0 || Shelly.getPuntosVida() <= 0){
+				break;
+			}
+			else{
+				Descanso(Colt);
+			}
+			
+		}
+
+	}
+		// =========================================================================
+        // Ejercicio 6 
+        // =========================================================================
+	public static void CalcularDanio(Peleador atacante, Peleador defensor, Ataque ataque) {
+
+
+		// =========================================================================
+        // Ejercicio 7
+        // =========================================================================
+		
+		if(atacante.getEnergia() >= ataque.getCostoEnergia()){
+			int danioTotal = ataque.getDanioBase() + atacante.getArmas().get(0).getBonificadorDanio().intValue();
+    	
+			int danioRecibido = danioTotal - defensor.getDefensaBase().intValue();
+    
+    		if (danioRecibido < 0) {
+        	danioRecibido = 0;
+    		}
+		
+    		defensor.setPuntosVida(defensor.getPuntosVida() - danioRecibido);
+
+			System.out.println( atacante.getNombre() + " hizo " + danioRecibido + " de daño.");
+		
+			if (defensor.getPuntosVida() < 0) {
+				System.out.println(defensor.getNombre() + " murió.");
+    		}
+			else{
+				System.out.println("A " + defensor.getNombre() + " le quedo " + defensor.getPuntosVida() + " de vida.");
+			}
+			atacante.setEnergia(atacante.getEnergia() - ataque.getCostoEnergia());
+			System.out.println(atacante.getNombre() + " tiene " + atacante.getEnergia() + " de energía restante.");
+		}
+		else{
+			System.out.println(atacante.getNombre() + " está demasiado cansado para atacar");
+		}
+		
+	}
+
+	// =========================================================================
+    // Ejercicio 8
+    // =========================================================================
+	
+	public static void Descanso(Peleador peleador){
+		if (peleador.getPuntosVida() < 200){
+			peleador.setPuntosVida(peleador.getPuntosVida() + 500);
+			peleador.setEnergia(0);
+			System.out.println(peleador.getNombre() + " ha descansado y ahora tiene "+ peleador.getPuntosVida() + " puntos de vida y " + peleador.getEnergia() + " de energía.");
+		}
+
+		else{
+			peleador.setEnergia(peleador.getEnergia() + 100);
+			System.out.println(peleador.getNombre() + " ha descansado y ahora tiene " + peleador.getEnergia() + " de energía.");
+		}
+	}
+	// =========================================================================
+    // Ejercicio 9
+    // =========================================================================
+	//Usamos una funcion ya que en el ejercicio no aclara bien si puede sers cualquier peleador o uno en especifico, entonces se hace una funcion que reciba un peleador y filtre sus habilidades dependiendo de su energia actual.
+    public static void FiltrarHabilidades(Peleador peleador) {
+        System.out.println("Ataques disponibles para " + peleador.getNombre() + " (Energía actual: " + peleador.getEnergia() + "):");
+        
+        for (Ataque ataque : peleador.getAtaques()) {
+            if (ataque.getCostoEnergia() <= peleador.getEnergia()) {
+                System.out.println("- " + ataque.getNombre() + " (Costo energía: " + ataque.getCostoEnergia() + ")");
+            }
+        }
     }
 	
 }
