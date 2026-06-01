abcabcab2b5848c86d219cdd65b7f90992afe3a7
tbressia@gmail.com <tbressia@gmail.com>
2026-06-01 10:36:24 -0300

    arrgelo de todo

--- src/main/java/Starapi/entidades/Arma.java
+++ src/main/java/Starapi/entidades/Arma.java
@@ -50,43 +50,4 @@ public class Arma {
     public float getPeso() {
     return peso;
     }
-
-    public Arma(Long id, String nombre, Long bonificadorDanio, float peso) {
-        this.id = id;
-        this.nombre = nombre;
-        this.bonificadorDanio = bonificadorDanio;
-        this.peso = peso;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setBonificadorDanio(Long bonificadorDanio) {
-        this.bonificadorDanio = bonificadorDanio;
-    }
-
-    public void setPeso(float peso) {
-        this.peso = peso;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public Long getBonificadorDanio() {
-        return bonificadorDanio;
-    }
-
-    public float getPeso() {
-        return peso;
-    }
 }
