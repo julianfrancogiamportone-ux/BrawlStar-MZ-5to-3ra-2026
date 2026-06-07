6f979dc0aca07eb694f13af89b346d69c9bcf7bc
tbressia@gmail.com <tbressia@gmail.com>
2026-06-07 17:28:39 -0300

    arreglotomy1

--- src/main/java/Starapi/controller/PeleadorController.java
+++ src/main/java/Starapi/controller/PeleadorController.java
@@ -1,5 +1,28 @@
 package Starapi.controller;
-
-public class PeleadorController {
-
-}
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.PostMapping; 
+import org.springframework.web.bind.annotation.RequestBody; 
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RestController; 
+import Starapi.IService.PeleadorIservice; 
+import Starapi.entidades.Peleador;
+import java.util.List;
+ 
+@RestController 
+@RequestMapping("/api/characters") 
+public class PeleadorController { 
+ 
+    @Autowired 
+    private PeleadorIservice pService; 
+ 
+    @GetMapping 
+    public List<Peleador> getAllCharacters() { 
+        return pService.findAllCharacters(); 
+    } 
+ 
+    @PostMapping 
+    public Peleador createCharacter(@RequestBody Peleador peleador) { 
+        return pService.saveCharacter(peleador); 
+    } 
+} 
